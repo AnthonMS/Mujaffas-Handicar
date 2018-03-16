0e016299bc8f7c7adb69aea98d2baae6c9b4efab
AnthonMS <Anthon@Steiness.info>
2018-03-16 15:49:05 +0100

    Background music and Mute button
    I have added background music. It is the Gold for cash song, and I tried to loop it as good as I could.
    I have also added a mute button in the top right corner because people might get annoyed quickly, I will have to figure out how to add the mute setting in a constant variable so you don't have to mute it every time the scene restarts or you start the game.

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    private int muteTier = 0;
+    private GameObject audioMan;
+    private AudioSource effectsSrc; // AudioSource 0
+    private AudioSource motorSrc; // AudioSource 1
+    private AudioSource musicSrc; // AudioSource 2
+
+    [Header("Button Stuff")]
+    public Button muteBtn;
+    public Sprite speakerFullIcon;
+    public Sprite speakerLowIcon;
+    public Sprite speakerMuteIcon;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Debug.Log("Initialize button manager");
+        audioMan = GameObject.FindGameObjectWithTag("AudioManager");
+        effectsSrc = audioMan.GetComponents<AudioSource>()[0];
+        motorSrc = audioMan.GetComponents<AudioSource>()[1];
+        musicSrc = audioMan.GetComponents<AudioSource>()[2];
+	}
+
+    // Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    public void MuteOrUnmute()
+    {
+        //Debug.Log("Mute or Unmute audio");
+        if (muteTier == 0)
+        {
+            Debug.Log("Mute Music!");
+            musicSrc.mute = true;
+            //musicSrc.Stop();
+            muteBtn.GetComponent<Image>().sprite = speakerLowIcon;
+            muteTier = 1;
+        }
+        else if (muteTier == 1)
+        {
+            Debug.Log("Mute Music AND effects");
+            musicSrc.mute = true;
+            motorSrc.mute = true;
+            effectsSrc.mute = true;
+            muteBtn.GetComponent<Image>().sprite = speakerMuteIcon;
+            muteTier = 2;
+        }
+        else if (muteTier == 2)
+        {
+            Debug.Log("Unmute Music AND effects");
+            musicSrc.mute = false;
+            //musicSrc.Play();
+            motorSrc.mute = false;
+            effectsSrc.mute = false;
+            muteBtn.GetComponent<Image>().sprite = speakerFullIcon;
+            muteTier = 0;
+        }
+    }
+}
